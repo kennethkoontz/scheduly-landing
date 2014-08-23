745370468b220cd8952f58011666936cb8458875
kenneth koontz <kenneth.koontz@gmail.com>
2014-08-23 16:49:54 -0700

    initial commit

---
+++ StyleSwitcher/switcher.js
@@ -0,0 +1,24 @@
+/*global $, jQuery, switchStylestyle, google*/
+
+$('#StyleSwitcher .switcher-btn').click(function () {
+
+    'use strict';
+
+    $('#StyleSwitcher').toggleClass('open');
+
+    return false;
+});
+
+
+
+
+$('.color-switch').click(function () {
+
+    'use strict';
+
+    var title = jQuery(this).attr('title');
+    jQuery('#color-switch').attr('href', 'css/colors/' + title + '.css');
+
+    return false;
+
+});
