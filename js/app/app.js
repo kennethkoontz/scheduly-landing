dfe3104d6f9461038e586d703962348287f44602
ken koontz <kenneth.koontz@gmail.com>
2014-08-24 00:45:31 -0700

    add ui router

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,15 @@
-angular.module('scheduly', [])
-  .controller('MarketingCtrl', function($scope) {
+angular.module('scheduly', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: 'partials/landing.html',
+        controller: 'LandingCtrl'
+      })
 
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
+
+  }])
+  .controller('LandingCtrl', function($scope) {
   });
