c6219682ac2a402b4f7e07031bb83749b5ddd529
ken koontz <kenneth.koontz@gmail.com>
2014-08-23 23:36:52 -0700

    remove testimonials

--- sync-with-s3.sh
+++ sync-with-s3.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 S3_BUCKET=scheduly
-echo "Deploying to S3..."
-s3cmd sync --delete-removed --acl-public --rexclude '.git\/*|hooks\/' ./ s3://$S3_BUCKET
+echo "Syncing with S3..."
+s3cmd sync --cf-invalidate --delete-removed --acl-public --rexclude '.git\/*|sync-with-s3.sh' ./ s3://$S3_BUCKET
+echo "Done..."
