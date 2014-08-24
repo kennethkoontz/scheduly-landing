302a6f00c86448f4da6f90a3fb0d429970862f11
ken koontz <kenneth.koontz@gmail.com>
2014-08-24 00:01:29 -0700

    fix up syncing

--- sync-with-s3.sh
+++ sync-with-s3.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 S3_BUCKET=scheduly
+
+cat .gitignore .s3ignore > .allignore
+cat .allignore
+
 echo "Syncing with S3..."
-s3cmd sync --cf-invalidate --delete-removed --acl-public --rexclude '.git\/*|sync-with-s3.sh' ./ s3://$S3_BUCKET
+
+s3cmd sync --cf-invalidate --delete-removed --acl-public --exclude-from .allignore ./ s3://$S3_BUCKET
+
+rm -f .allignore
+
 echo "Done..."
